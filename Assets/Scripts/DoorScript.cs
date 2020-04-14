6f4aaa3c649847365c0a5a69b6ab905fe3bcca5d
Ishan Dutta <ali3nator5000@gmail.com>
2020-04-14 14:56:16 -0500

    Pushed essential updates
    Door animation
    Raycasting for selecting items
    Need to fix a bit of the raycasting

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.enabled = false;
+    }
+
+    void pauseAnimation()
+    {
+        anim.enabled = false;
+    }
+}
---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    public Camera camera;
+    [SerializeField] private Material highlightMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            var selectionRenderer = selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = highlightMaterial;
+            }
+        }
+    }
+}
