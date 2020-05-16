e22f04d21a4d9e55f96b5531b0ec9fe75212442c
Ishan Dutta <ali3nator5000@gmail.com>
2020-05-15 23:37:45 -0500

    Final Build commit

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void StartupScene()
+    {
+        SceneManager.LoadScene("Startup");
+    }
+
+    public void MainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
